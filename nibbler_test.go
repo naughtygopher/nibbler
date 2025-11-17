c1e2c7645d6cc8241b9c59fe7f1f0cbeee3aa068
Kamaleshwar <bn_kamalesh@yahoo.com>
2025-11-17 17:07:42 +0100

    nolint

--- nibbler_test.go
+++ nibbler_test.go
@@ -161,7 +161,7 @@ func TestProcessorErr(tt *testing.T) {
 		nib, err := Start(&Config[string]{
 			TickerDuration: time.Second,
 			Processor: func(_ context.Context, _ Trigger, _ []string) error {
-				panic(errProcessing)
+				panic(errProcessing) //nolint:gocritic
 			},
 			ProcessorErr: func(failedBatch []string, err error) {
 				asserter.ErrorIs(err, errProcessing)
